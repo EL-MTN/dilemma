78312a32466bc57e4aef254ba0be460867b18c9b
Eric Li <ericlmtn@gmail.com>
2024-07-15 15:49:08 -0400

    Prevent conflict with nextjs https deployment

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
+			<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
 			<body className={inter.className}>
 				<Providers>
 					<Navbar />
